b5a2fa387032aa73b6919926d46ad25b1f3adc77
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-09-27 10:48:47 +0500

    project6 --- Initialized

---
+++ project6/src/main/java/com/demo/dataSourceBean/DataSourceBeanApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dataSourceBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSourceBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSourceBeanApplication.class, args);
+	}
+
+}
---
+++ project6/src/test/java/com/demo/dataSourceBean/DataSourceBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.dataSourceBean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSourceBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
